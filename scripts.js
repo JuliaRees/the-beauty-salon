8b3d1078cb359bac49aef281c2476ad39ce01bec
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-08-22 14:09:48 +0800

    last-fixes

--- scripts.js
+++ scripts.js
@@ -19,9 +19,9 @@ var typed = new Typed('#typed', {
 	stringsElement: '#typed-strings',
   	typeSpeed: 60,
   	startDelay: 500,
-  	backSpeed: 30,
-  	backDelay: 1000,
-  	smartBackspace: true,
-  	loop: true,
-  	loopCount: Infinity
+  	//backSpeed: 30,
+  	//backDelay: 1000,
+  	//smartBackspace: false,
+  	//loop: true,
+  	//loopCount: Infinity
 });
